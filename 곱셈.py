6f925150b06e234925fd7145f6c8f6dbabb71272
nemo <nemo724@naver.com>
2024-09-17 22:27:11 +0900

    연산 값들을 저장하는 변수와 b의 각 자리수를 추출하는 로직 작성

--- 곱셈.py
+++ 곱셈.py
@@ -1,1 +1,11 @@
 a,b=int(input()).split()
+total=0
+
+for i in range (1,2,1):
+    output_b=b%10
+    output_result=a*output_b
+    print(f"{output_result}")
+    b=b//10
+    total=total+output_result
+
+print(f"{total}")
