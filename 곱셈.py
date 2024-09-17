1905bc34a5805b9cfe6b3946e1bff3e5b9cf38b0
nemo <nemo724@naver.com>
2024-09-17 22:36:43 +0900

    입력 함수 수정 및 total 계산 수정

--- 곱셈.py
+++ 곱셈.py
@@ -1,11 +1,11 @@
-a,b=int(input()).split()
+a,b=map(int, input().split())
 total=0
 
-for i in range (1,2,1):
+for i in range (0,3,1):
     output_b=b%10
     output_result=a*output_b
     print(f"{output_result}")
     b=b//10
-    total=total+output_result
+    total=total+(output_result*(10**i))
 
 print(f"{total}")
